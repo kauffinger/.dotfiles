df2d8472c8151d688e2080943b5411ed69f415e2
Konstantin Auffinger <konstantinauffinger@gmail.com>
2024-11-18 20:41:11 +0100

    fix: make wezterm font rendering look like kitty

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,15 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
 
 config.color_scheme = "Catppuccin Mocha"
 
-config.font = wezterm.font("MonoLisa Nerd Font Mono")
-config.font_size = 13
+config.font = wezterm.font("MonoLisa Nerd Font Mono", { weight = "Bold" })
+config.font_size = 12
+config.freetype_load_flags = "NO_HINTING"
+config.front_end = "OpenGL"
+config.freetype_load_target = "Light"
+config.freetype_render_target = "HorizontalLcd"
 
 config.enable_tab_bar = false
 
@@ -17,5 +17,4 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
 
--- and finally, return the configuration to wezterm
 return config
